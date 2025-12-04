cd8b4c37bb5cc601c112ecb2a9963bcfc87bb783
SohelShahid117 <sohelshahid09@gmail.com>
2025-12-05 02:53:05 +0600

    navbar done-5 min

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center"></div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
