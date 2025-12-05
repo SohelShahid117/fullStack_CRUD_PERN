6f6d1df092bf454380e2b43c71502ef9f769113e
SohelShahid117 <sohelshahid09@gmail.com>
2025-12-05 20:20:10 +0600

    now coding on database-50 min

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  //   return res.json({ message: "Hello" });
+  return res.send("<h1>Hello sohel</h1>");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`server is running at http://localhost:${port} `);
+});
